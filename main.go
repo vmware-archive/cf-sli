9a6072b91daf4a79ac4a43ca2bee0f3912d6b88e
Natalie J. Bennett <nbennett@pivotal.io>
2017-04-13 14:25:16 -0700

    extract wait_for_cf to a function and report the route and start/stop time
    [#142811493] program that can cf push an app with one command and get a unique route back
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	uuid "github.com/nu7hatch/gouuid"
+	gexec "github.com/onsi/gomega/gexec"
 	"github.com/pivotal-cloudops/cf-sli/config"
 )
+
+func wait_for_cf(context *gexec.Session, cf_command string) {
+	_ = <-context.Exited
+	if context.ExitCode() != 0 {
+		fmt.Println("cf " + cf_command + " failed")
+		os.Exit(1)
+	}
+}
 
 func main() {
 	var c config.Config
@@ -18,40 +28,35 @@ func main() {
 	}
 
 	context := cf.Cf("api", c.Api)
-	_ = <-context.Exited
-
-	if context.ExitCode() != 0 {
-		fmt.Println("cf api failed")
-		os.Exit(1)
-	}
+	wait_for_cf(context, "api")
 
 	context = cf.Cf("auth", c.User, c.Password)
-	_ = <-context.Exited
-
-	if context.ExitCode() != 0 {
-		fmt.Println("cf auth failed")
-		os.Exit(1)
-	}
+	wait_for_cf(context, "auth")
 
 	context = cf.Cf("target", "-o", c.Org, "-s", c.Space)
-	_ = <-context.Exited
-
-	if context.ExitCode() != 0 {
-		fmt.Println("cf target failed")
-		os.Exit(1)
-	}
+	wait_for_cf(context, "target")
 
 	guid, err := uuid.NewV4()
 	if err != nil {
 		panic(err)
 	}
 
-	context = cf.Cf("push", "-p", "./assets/ruby_simple", guid.String()[0:20], "--no-start")
-	_ = <-context.Exited
+	app_name := guid.String()[0:20]
 
-	if context.ExitCode() != 0 {
-		fmt.Println("cf push failed")
-		os.Exit(1)
-	}
+	context = cf.Cf("push", "-p", "./assets/ruby_simple", app_name, "--no-start")
+	wait_for_cf(context, "push")
 
+	start := time.Now()
+	context = cf.Cf("start", app_name)
+	wait_for_cf(context, "start")
+	cf_start_elapsed := time.Since(start)
+
+	start = time.Now()
+	context = cf.Cf("stop", app_name)
+	wait_for_cf(context, "stop")
+	cf_stop_elapsed := time.Since(start)
+
+	fmt.Printf("App route: %s.cfapps.io\n", app_name)
+	fmt.Printf("App start time: %s\n", cf_start_elapsed)
+	fmt.Printf("App stop time: %s\n", cf_stop_elapsed)
 }
