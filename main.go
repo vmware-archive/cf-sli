e1aa3a4c9ca4860e0d440ecd44371c7eb1050a1b
Kai Xiang <kxiang@pivotal.io>
2017-04-13 11:01:53 -0700

    Push the test app with no start with cf-test-helpers
    [#142811493] program that can cf push an app with one command and get a unique route back
    
    Signed-off-by: Natalie J. Bennett <nbennett@pivotal.io>

--- main.go
+++ main.go
@@ -1,4 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	uuid "github.com/nu7hatch/gouuid"
+	"github.com/pivotal-cloudops/cf-sli/config"
+)
+
 func main() {
+	var c config.Config
+	err := c.LoadConfig("./.config")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	context := cf.Cf("api", c.Api)
+	_ = <-context.Exited
+
+	if context.ExitCode() != 0 {
+		fmt.Println("cf api failed")
+		os.Exit(1)
+	}
+
+	context = cf.Cf("auth", c.User, c.Password)
+	_ = <-context.Exited
+
+	if context.ExitCode() != 0 {
+		fmt.Println("cf auth failed")
+		os.Exit(1)
+	}
+
+	context = cf.Cf("target", "-o", c.Org, "-s", c.Space)
+	_ = <-context.Exited
+
+	if context.ExitCode() != 0 {
+		fmt.Println("cf target failed")
+		os.Exit(1)
+	}
+
+	guid, err := uuid.NewV4()
+	if err != nil {
+		panic(err)
+	}
+
+	context = cf.Cf("push", "-p", "./assets/ruby_simple", guid.String()[0:20], "--no-start")
+	_ = <-context.Exited
+
+	if context.ExitCode() != 0 {
+		fmt.Println("cf push failed")
+		os.Exit(1)
+	}
+
 }
